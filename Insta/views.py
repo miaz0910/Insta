33fd9997c0178dfb71a4d0f9d2828607dfec08d3
mia <mia@mias-MBP.attlocal.net>
2020-03-22 22:40:20 -0700

    Display static page

--- Insta/views.py
+++ Insta/views.py
@@ -2,6 +2,8 @@ from django.views.generic import TemplateView, ListView,DetailView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.urls import reverse,reverse_lazy
 from Insta.models import Post
+from Insta.forms import CustomUserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 class HelloWorld(TemplateView):
     template_name='test.html'
@@ -14,10 +16,11 @@ class PostDetailView(DetailView):
     model=Post
     template_name='post_detail.html'
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin, CreateView):
     model=Post
     template_name='post_create.html'
     fields = '__all__'
+    login_url='login'
 
 class PostUpdateView(UpdateView):
     model=Post
@@ -28,3 +31,8 @@ class PostDeleteView(DeleteView):
     model=Post
     template_name='post_delete.html'
     success_url=reverse_lazy("posts")
+
+class SignUp(CreateView):
+    form_class=CustomUserCreationForm
+    success_url=reverse_lazy("login")
+    template_name='signup.html'
