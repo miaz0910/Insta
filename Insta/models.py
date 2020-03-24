d488d3e8247673986a0124021d41a864904ecd0b
mia <mia@mias-MBP.attlocal.net>
2020-03-23 21:57:09 -0700

    Display static page

--- Insta/models.py
+++ Insta/models.py
@@ -4,7 +4,34 @@ from django.contrib.auth.models import AbstractUser
 from imagekit.models import ProcessedImageField
 
 # Create your models here.
+class InstaUser(AbstractUser):
+    profile_pic=ProcessedImageField(
+        upload_to='static/images/profiles',
+        format='JPEG',
+        options={'quality':100},
+        blank=True,
+        null=True,
+    )
+
+    def get_connections(self):
+        connections = UserConnection.objects.filter(creator=self)
+        return connections
+
+    def get_followers(self):
+        followers = UserConnection.objects.filter(following=self)
+        return followers
+
+    def is_followed_by(self, user):
+        followers = UserConnection.objects.filter(following=self)
+        return followers.filter(creator=user).exists()
+
+
 class Post(models.Model):
+    author=models.ForeignKey(
+        InstaUser,
+        on_delete=models.CASCADE,
+        related_name='my_posts'
+    )
     title=models.TextField(blank=True,null=True)
     image=ProcessedImageField(
         upload_to='static/images/posts',
@@ -15,13 +42,39 @@ class Post(models.Model):
     )
 
     def get_absolute_url(self):
-        return reverse("post_detail",args=[str(self.id)])
+        return reverse("post_detail", args=[str(self.id)])
     
-class InstaUser(AbstractUser):
-    profile_pic=ProcessedImageField(
-        upload_to='static/images/profiles',
-        format='JPEG',
-        options={'quality':100},
-        blank=True,
-        null=True,
+class UserConnection(models.Model):
+    created = models.DateTimeField(auto_now_add=True, editable=False)
+    creator = models.ForeignKey(
+        InstaUser,
+        on_delete=models.CASCADE,
+        related_name="friendship_creator_set")
+    following = models.ForeignKey(
+        InstaUser,
+        on_delete=models.CASCADE,
+        related_name="friend_set")
+
+    def __str__(self):
+        return self.creator.username + ' follows ' + self.following.username
+
+
+class Like(models.Model):
+    post =models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name='likes'
     )
+
+    user =models.ForeignKey(
+        InstaUser,
+        on_delete=models.CASCADE,
+        related_name='likes'
+    )
+
+    class Meta:
+        unique_together=("post", "user")
+
+    def __str__(self):
+        return 'Like: ' + self.user.username +' like ' + self.post.title
+    
--- Insta/views.py
+++ Insta/views.py
@@ -1,9 +1,15 @@
-from django.views.generic import TemplateView, ListView,DetailView
-from django.views.generic.edit import CreateView, DeleteView, UpdateView
-from django.urls import reverse,reverse_lazy
-from Insta.models import Post
-from Insta.forms import CustomUserCreationForm
+
+from annoying.decorators import ajax_request
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.views.generic import DetailView, ListView, TemplateView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+
+
+from .forms import CustomUserCreationForm
+from .models import InstaUser, Post, Like,UserConnection
+
 
 class HelloWorld(TemplateView):
     template_name='test.html'
@@ -12,9 +18,20 @@ class PostsView(ListView):
     model=Post
     template_name='index.html'
 
+    def get_queryset(self):
+        current_user = self.request.user
+        following = set()
+        for conn in UserConnection.objects.filter(creator=current_user).select_related('following'):
+            following.add(conn.following)
+        return Post.objects.filter(author__in=following)
+
 class PostDetailView(DetailView):
     model=Post
     template_name='post_detail.html'
+
+class UserDetailView(DetailView):
+    model=InstaUser
+    template_name='user_detail.html'
 
 class PostCreateView(LoginRequiredMixin, CreateView):
     model=Post
@@ -36,3 +53,21 @@ class SignUp(CreateView):
     form_class=CustomUserCreationForm
     success_url=reverse_lazy("login")
     template_name='signup.html'
+
+@ajax_request
+def addLike(request):
+    post_pk = request.POST.get('post_pk')
+    post = Post.objects.get(pk=post_pk)
+    try:
+        like = Like(post=post, user=request.user)
+        like.save()
+        result = 1
+    except Exception as e:
+        like = Like.objects.get(post=post, user=request.user)
+        like.delete()
+        result = 0
+
+    return {
+        'result': result,
+        'post_pk': post_pk
+    }
